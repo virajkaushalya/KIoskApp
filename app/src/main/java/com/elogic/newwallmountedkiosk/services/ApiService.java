4976a5c2b111670b06d3fa2b79062e22a330c169
Viraj Kaushalya <viraj012003@gmail.com>
2026-07-14 20:27:05 +0530

    change api service

--- app/src/main/java/com/elogic/newwallmountedkiosk/services/ApiService.java
+++ app/src/main/java/com/elogic/newwallmountedkiosk/services/ApiService.java
@@ -7,7 +7,6 @@ import com.elogic.newwallmountedkiosk.responses.LoginResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface ApiService {
